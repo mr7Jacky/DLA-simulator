7543c0dc7eb04352c592b5560c39c2fbd5537ad8
Jacky Lin <mr7.jacky@gmail.com>
2021-03-25 17:14:24 -0400

    fix bugs on export image

--- off_lattice/StaticOffLatticeCluster.py
+++ off_lattice/StaticOffLatticeCluster.py
@@ -1,12 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import off_lattice.helper as hp
 from off_lattice.Particle import Particle
 
 
 class StaticOffLatticeCluster:
 
-    def __init__(self, particle_radius=1, lattice_size=500, delta=2 * np.pi, max_num_particle=3000):
+    def __init__(self, particle_radius=1, lattice_size=500, delta=2 * np.pi, max_num_particle=3000, alpha=1):
         self.lattice_size = lattice_size
         self.mid = self.lattice_size // 2
         self.cluster = []
@@ -15,6 +14,7 @@ class StaticOffLatticeCluster:
         self.max_num_particle = max_num_particle
         self.step_limit = 10000
         self.delta = delta
+        self.alpha = alpha
         self.particle_radius = particle_radius
 
         self.put_init_seed()
@@ -33,7 +33,6 @@ class StaticOffLatticeCluster:
         # loop over particles
         num_part_launch = 0
         while (self.num_particle <= self.max_num_particle) and (2 * self.r_max < self.mid - 2):
-
             print("npart= ", self.num_particle, " total_launch: ", num_part_launch)
             num_part_launch += 1
             x, y = self.generate_random_point()
@@ -45,7 +44,7 @@ class StaticOffLatticeCluster:
                 # determine distance of random walker from midpoint of lattice
                 r = np.sqrt((x - self.mid) ** 2 + (y - self.mid) ** 2)
                 # stop random walk if out of boundary
-                if r >= 2 * self.r_max:
+                if r >= 2 * self.particle_radius * self.r_max:
                     walk_stop = 1
                 # stop random walk if next to cluster site
                 if self.near_cluster(x, y):
@@ -62,7 +61,7 @@ class StaticOffLatticeCluster:
         """
         # Check the square circumscribing the particle
         for particle in self.cluster:
-            if hp.cal_two_points_dist(particle.loc, (x, y)) <= self.particle_radius:
+            if self.cal_two_points_dist(particle.loc, (x, y)) <= 2 * self.particle_radius:
                 return True
         return False
 
@@ -73,19 +72,23 @@ class StaticOffLatticeCluster:
         """
         # start new random walker on circle with radius (RMAX+2)  (rule II)
         theta_rand = 2.0 * np.pi * np.random.random_sample()
-        x = self.mid + ((self.r_max + 2) * np.cos(theta_rand))
-        y = self.mid + ((self.r_max + 2) * np.sin(theta_rand))
+        x = self.mid + ((self.particle_radius * self.r_max + 2) * np.cos(theta_rand))
+        y = self.mid + ((self.particle_radius * self.r_max + 2) * np.sin(theta_rand))
         return x, y
 
     def cal_next_move(self, x, y, phi):
         eta = np.random.uniform(low=-self.delta/2, high=self.delta/2)
         new_phi = phi + eta
-        alpha = 2
-        new_x = x + int(alpha * np.cos(new_phi))
-        new_y = y + int(alpha * np.sin(new_phi))
+        new_x = x + self.alpha * np.cos(new_phi)
+        new_y = y + self.alpha * np.sin(new_phi)
         return new_x, new_y, phi
 
+    @staticmethod
+    def cal_two_points_dist(loc1, loc2):
+        return np.sqrt(np.power(loc1[0] - loc2[0], 2) + np.power(loc1[1] - loc2[1], 2))
+
     def plot_cluster(self, out_name):
+        plt.figure()
         plt.title("DLA Cluster", fontsize=20)
         for particle in self.cluster:
             circle = plt.Circle(particle.loc, particle.radius, fc='white', ec="black")
@@ -97,6 +100,6 @@ class StaticOffLatticeCluster:
 
 
 if __name__ == "__main__":
-    c = StaticOffLatticeCluster(particle_radius=1, lattice_size=500, delta=2 * np.pi, max_num_particle=3000)
+    c = StaticOffLatticeCluster(particle_radius=1, lattice_size=500, delta=2 * np.pi, max_num_particle=300, alpha=1)
     c.simulate()
     c.plot_cluster("images/off_lattice_" + str(c.num_particle) + ".png")
--- off_lattice/helper.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from typing import List
-
-from off_lattice.Particle import Particle
-
-
-def cal_two_points_dist(loc1, loc2):
-    return np.sqrt(np.power(loc1[0] - loc2[0], 2) + np.power(loc1[1] - loc2[1], 2))
